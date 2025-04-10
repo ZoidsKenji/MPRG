b33973de20d9873d7fecd278a28f84648da91b04
ZoidsKenji <chunkinmak123@gmail.com>
2025-04-10 08:49:16 +0100

    10/4 commit

--- MPRG/obj/Debug/net8.0-windows/MPRG.AssemblyInfo.cs
+++ MPRG/obj/Debug/net8.0-windows/MPRG.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MPRG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbc92ab897a9daf9f6e19fb2c3c59f5473defee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MPRG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MPRG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
