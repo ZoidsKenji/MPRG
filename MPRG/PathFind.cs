5711d8c2ed7f26251a5a042d7a74a1172509b3dc
ZoidsKenji <chunkinmak123@gmail.com>
2025-06-23 23:23:44 +0100

    23/6 commit

--- MPRG/PathFind.cs
+++ MPRG/PathFind.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using Microsoft.Xna.Framework;
@@ -14,14 +15,30 @@ namespace MPRG
         public int height;
         public int width;
 
-        public List<PathNode> nodes;
+        public List<List<PathNode>> nodes;
 
         public PathFind(List<List<int>> map)
         {
             this.map = map;
             height = map.Count;
             width = map[0].Count;
-            nodes = new List<PathNode>();
+            nodes = new List<List<PathNode>>();
+
+            for (int y = 0; y < height; y++){
+                List<PathNode> row = [];
+                for (int x = 0; x < width; x++){
+                    bool drivible = map[x][y] == 0;
+                    row.Add(new PathNode(x, y, drivible));
+                }
+                nodes.Add(row);
+            }
+        }
+
+        public PathNode getNode(int x, int y){
+            if (0 <= x && x < width && 0 <= y && y < height){
+                return nodes[x][y];
+            }
+            return null;
         }
     }
 }
